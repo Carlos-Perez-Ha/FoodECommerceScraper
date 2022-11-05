de2b4a3a114b2d6bdf91873f8db4ada3da638a9c
carlos-perez-ha <cperezha@uoc.edu>
2022-11-05 08:14:02 +0100

    Primera version de cargar las paginas de producto

--- source/main.py
+++ source/main.py
@@ -1,25 +1,11 @@
-import requests
-from bs4 import BeautifulSoup
-import re
-import builtwith
-import whois
-
-URL = 'tienda.mercadona.es'
-
-
-def mercadona_scraping():
-
-    page = requests.get('http://' + URL)
-
-    soup = BeautifulSoup(page.content, features='html.parser')
-
-    pass
-
+import DiaScraper
 
 if __name__ == '__main__':
 
-    mercadona_scraping()
+    diaScraper = DiaScraper.DiaScraper()
 
-    print(builtwith.builtwith('http://'+URL))
+    diaScraper.cargar_paginas_producto()
 
-    print(whois.whois('http://www.mercadona.com'))
+    #print(builtwith.builtwith('http://'+URL))
+
+    #print(whois.whois('http://www.mercadona.com'))
