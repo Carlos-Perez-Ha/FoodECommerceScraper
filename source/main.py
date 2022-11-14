2d6a5d8a9542f2bed0113dc931376219cc8f9be4
opromio <oscar.promio@enzymeadvisinggroup.com>
2022-11-14 17:29:53 +0100

    Changed paths for output data & date formats

--- source/main.py
+++ source/main.py
@@ -1,12 +1,8 @@
 import logging
 from DiaScraper import DiaScraper
 
-logging.basicConfig(level=logging.INFO)
-# filename='log.log', filemode='w',
-
-
 if __name__ == '__main__':
     diaScraping = DiaScraper()
-    diaScraping.start_scraping(reload=False)
+    # diaScraping.start_scraping(reload=False)
     diaScraping.parse_results()
     logging.info("Process finished.")
