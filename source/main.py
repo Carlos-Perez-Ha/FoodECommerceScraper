90973718f845fe4f4b2159a04067b41854f707e7
opromio <oscar.promio@enzymeadvisinggroup.com>
2022-11-14 17:34:59 +0100

    refactoring

--- source/main.py
+++ source/main.py
@@ -3,6 +3,6 @@ from DiaScraper import DiaScraper
 
 if __name__ == '__main__':
     diaScraping = DiaScraper()
-    # diaScraping.start_scraping(reload=False)
+    diaScraping.start_scraping(reload=False)
     diaScraping.parse_results()
     logging.info("Process finished.")
